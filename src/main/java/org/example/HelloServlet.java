1f6e90e121543ba76bc28b6ec9c5210cda90c7c5
Denis Bazhin <den.bazhin.2017@mail.ru>
2024-09-24 23:00:43 +0500

    commit

---
+++ src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().write("<h1>Hello, World!</h1>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+    <h2>Welcome to My Web Application</h2>
+    <p>Go to <a href="hello">Hello Servlet</a> to see the servlet in action.</p>
+</body>
+</html>
