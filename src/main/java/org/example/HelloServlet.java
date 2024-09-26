5fd11012b97a0eaf8b01350cf0942996ea264d46
Denis Bazhin <den.bazhin.2017@mail.ru>
2024-09-26 10:26:56 +0500

    Updated HelloServlet message

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -9,6 +9,6 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("<h1>Helloоооощ!</h1>");
+        resp.getWriter().write("<h1>Hello!</h1>");
     }
 }
