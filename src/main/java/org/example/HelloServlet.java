35dae3ee2f7dc81e1cc8b10c73af3cffe2b2a4df
Denis Bazhin <den.bazhin.2017@mail.ru>
2024-09-26 14:46:41 +0500

    Updated HelloServlet message

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -1,14 +1,15 @@
 package org.example;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("<h1>Hello Aston!</h1>");
+        resp.getWriter().println("<h1>Hello, World! This is a simple Servlet.</h1>");
     }
 }
